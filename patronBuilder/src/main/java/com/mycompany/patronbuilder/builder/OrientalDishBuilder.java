5cf1a527913b1a9fce532f2d8c14c00b8b3af971
Andres Rios <amrios@unicauca.edu.co>
2021-04-14 12:26:24 -0500

    Placeholder for SetCore()

--- patronBuilder/src/main/java/com/mycompany/patronbuilder/builder/OrientalDishBuilder.java
+++ patronBuilder/src/main/java/com/mycompany/patronbuilder/builder/OrientalDishBuilder.java
@@ -12,22 +12,28 @@ package com.mycompany.patronbuilder.builder;
 public class OrientalDishBuilder extends DishBuilder {
     
     @Override
-    public void setCore(){
+    public OrientalDishBuilder setCore(){
         
         this.setDish(new OrientalDish("Arroz Oriental", 
         "Arroz con estilo oriental, añadiendo ingredientes con sabores orientales como la soja, las verduras y las gambas",
         "", 56000, EnumSize.ALL, "East rice"));
-        
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
     
     @Override
-    public void addParts(){
-        
+    public OrientalDishBuilder addParts(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     @Override
-    public void setSize(){
-        
+    public OrientalDishBuilder setSize(){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public OrientalDishBuilder setDish(Dish dish) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
